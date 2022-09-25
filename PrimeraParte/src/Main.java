0bc4f57b2490a2787e4737707ee1126b6eb30b0f
Daniel Castañeda <110303810+Deca24@users.noreply.github.com>
2022-09-25 14:22:02 -0500

    Agregar primera parte del tema 3

---
+++ PrimeraParte/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(10, 30, 5);
+    }
+    public static void suma(int a, int b, int c){
+        int resultado = a+b+c;
+        System.out.println(resultado);
+    }
+}
